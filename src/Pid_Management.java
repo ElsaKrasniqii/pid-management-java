4da9baa5050decabde526ed39e65de02254e6a11
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-05-10 14:38:53 +0200

    Shtimi i tri variablave statike MIN_PID, MAX_PID dhe pidMap

--- src/Pid_Management.java
+++ src/Pid_Management.java
@@ -1,2 +1,10 @@
-public class Pid_Management {
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class Pid_Management{
+    private static final int MIN_PID = 300;
+    private static final int MAX_PID = 5000;
+    private static BitSet pidMap;
+
+
 }
