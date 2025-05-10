b032f73f43018056af873612a7f3a9f3ed6a1bc8
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-05-10 14:55:49 +0200

    Krijimi i funksionit isAllocated

--- src/Pid_Management.java
+++ src/Pid_Management.java
@@ -6,5 +6,12 @@ public class Pid_Management{
     private static final int MAX_PID = 5000;
     private static BitSet pidMap;
 
+    public static boolean isAllocated(int pid) {
+        if (pid < MIN_PID || pid > MAX_PID) {
+            throw new IllegalArgumentException("PID jashtë rangut të lejuar: " + pid);
+        }
+        return pidMap.get(pid - MIN_PID);
+    }
+
 
 }
